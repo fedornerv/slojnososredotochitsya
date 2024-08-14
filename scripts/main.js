578e1d099b59e2f1f23c94bbff05618a8d2dbb6a
feedor <9818092@mail.ru>
2024-08-14 16:40:05 +0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+//alert()
+console.log("3+5")
+console.log(3 + 5)
+//типы данных
+//числа Int 3667778
+//строки String, str
+
+//bool логика, true, false
+
+
+
+//переменные
+let x = 7;
+let y = 3;
+const z ="Привет";
+
+let t = x % y;
+
+console.log(t);
+
+let word = prompt("Введите свое имя");
+console.log("Привет" + word)
+//const
+//var
